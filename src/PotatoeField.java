051d8fe2413eb59a3ab2ddd02d8f2c9a72536e52
xlxl18 <tima_93@list.ru>
2017-08-21 12:52:25 +0600

    Update PotatoeField.java

--- src/PotatoeField.java
+++ src/PotatoeField.java
@@ -1,5 +1,12 @@
 import java.util.Date;
-public class PotatoeField  extends Field {
+public class PotatoeField  extends Field implements Sensor {
+    public static int humidityLevel= 250;
+    public void getLevel(int level) {
+        if (this.humidityLevel < level) {
+            Main main = new Main();
+            main.irrigation();
+        }
+    }
     public void startIrrigation() {
         System.out.println("Полив картофельного поля начал выполняться в: " + new Date());
     }
