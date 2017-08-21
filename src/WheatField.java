52a279d15a7de6182ea68b99945e32cda62275e8
xlxl18 <tima_93@list.ru>
2017-08-21 12:52:47 +0600

    Update WheatField.java

--- src/WheatField.java
+++ src/WheatField.java
@@ -1,5 +1,12 @@
 import java.util.Date;
-public class WheatField extends Field {
+public class WheatField extends Field implements Sensor {
+    public static int humidityLevel= 210;
+    public void getLevel(int level) {
+        if (this.humidityLevel < level) {
+            Main main = new Main();
+            main.irrigation();
+        }
+    }
     public void startIrrigation() {
         System.out.println("Полив кукурузного поля начал выполняться в: " + new Date());
     }
